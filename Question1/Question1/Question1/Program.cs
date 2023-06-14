af20a37cd59d3bb534eb4f17c0c9864b86bff57b
JiyanAtalay <135272170+JiyanAtalay@users.noreply.github.com>
2023-06-14 17:34:00 +0300

    Add files via upload

---
+++ Question1/Question1/Question1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace csdeneme
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 2520;
+
+            back:
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (n % i != 0)
+                {
+                    n++;
+                    goto back;
+                }
+            }
+            Console.WriteLine(n);
+
+            Console.ReadLine();
+        }
+    }
+}
